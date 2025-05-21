3dde34d4453bd85c1adfc9c03e59bee402f415a0
Francesco Palozzi <frapalozz@gmail.com>
2025-05-21 17:35:14 +0200

    Commit iniziale

---
+++ app/src/main/java/it/unicam/cs/mpgc/jbudget125914/App.java
@@ -0,0 +1,11 @@
+package it.unicam.cs.mpgc.jbudget125914;
+
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new it.unicam.cs.mpgc.jbudget125914.App().getGreeting());
+    }
+}
